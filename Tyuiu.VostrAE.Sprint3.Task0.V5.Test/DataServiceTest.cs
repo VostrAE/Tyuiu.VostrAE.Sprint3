0df0d8328bbd1419cc35cc343463b4b6f67fc65a
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-10-21 16:03:31 +0500

    Add project files.

---
+++ Tyuiu.VostrAE.Sprint3.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.VostrAE.Sprint3.Task0.V5.Lib;
+namespace Tyuiu.VostrAE.Sprint3.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int endValue = 10;
+            double res = ds.GetSumSeries(startValue, endValue);
+            double wait = 81.083;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
+
---
+++ Tyuiu.VostrAE.Sprint3.Task0.V5/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.VostrAE.Sprint3.Task0.V5.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Исходные данные:                                                         *");
+        Console.WriteLine("***************************************************************************");
+        int startValue = 1;
+        int endValue = 10;
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + endValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Разультат:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Cумма ряда = " + ds.GetSumSeries(startValue, endValue));
+        Console.ReadKey();
+    }
+}
+

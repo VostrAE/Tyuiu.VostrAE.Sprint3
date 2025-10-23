baf1a44eb9942de3b3779671d4f79e6bfc0885ed
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-10-23 23:53:12 +0500

    Task4

---
+++ Tyuiu.VostrAE.Sprint3.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.VostrAE.Sprint3.Task4.V3.Lib;
+namespace Tyuiu.VostrAE.Sprint3.Task4.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = -0.274;
+            Assert.AreEqual(res, wait);
+
+        }
+    }
+}
---
+++ Tyuiu.VostrAE.Sprint3.Task4.V3/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.VostrAE.Sprint3.Task4.V3.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Исходные данные:                                                         *");
+        Console.WriteLine("***************************************************************************");
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine("Старт шага: " + startValue);
+        Console.WriteLine("Конец шага: " + stopValue);
+
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Разультат:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Сумма ряда: " + ds.Calculate(startValue, stopValue));
+        Console.ReadKey();
+    }
+}

377c2d79fc55a9f84f936d8e0873529610b0d87e
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-10-24 21:09:06 +0500

    Task6

---
+++ Tyuiu.VostrAE.Sprint3.Task6.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.VostrAE.Sprint3.Task6.V22.Lib;
+namespace Tyuiu.VostrAE.Sprint3.Task6.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 19;
+            int stopValue = 31;
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 517;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VostrAE.Sprint3.Task6.V22/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.VostrAE.Sprint3.Task6.V22.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Исходные данные:                                                         *");
+        Console.WriteLine("***************************************************************************");
+        int startValue = 19;
+        int stopValue = 31;
+        Console.WriteLine("Старт шага суммы ряда: " + startValue);
+        Console.WriteLine("Конец шага суммы ряда: " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Разультат:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Сумма делителей: " + ds.GetSumTheDivisors(startValue, stopValue));
+        Console.ReadKey();
+    }
+}
